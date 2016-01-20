571211e87b685bffb1e71f7421a56c857a144233
pingswept <someone@somewhere.gov>
2016-01-20 16:01:11 -0500

    Fix zoom and center of map, remove dynamically generated file

--- mapserver.py
+++ mapserver.py
@@ -5,7 +5,7 @@ app = Flask(__name__)
 
 @app.route("/")
 def hello():
-	map_osm = folium.Map(location=[42.35341,-71.0829921])
+	map_osm = folium.Map(location=[42.398,-71.120], zoom_start=15)
 	map_osm.create_map(path='templates/map.html')
 	return render_template('index.html')
 
